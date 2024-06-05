76d0c2b77660bf299e19f4086cc17f36b01b9cb6
Saccharin01 <chousik01@gmail.com>
2024-06-05 12:03:03 +0900

    template literal refine

--- scope.js
+++ scope.js
@@ -13,12 +13,6 @@ const endPoint = "http://apis.data.go.kr/6300000/eventDataService";
 const keyName = `Me%2FMuO55x%2B9wjLDeYl9RoSbEbVNZuF1eg4GGtNtw0ezd8EzDfprXxSxoRQPRSxrKf45kzYf3jCWnPGyQNh2MIQ%3D%3D`;
 
 
-const example = `http://apis.data.go.kr/6300000/eventDataService/eventDataListJson
-?
-serviceKey=${keyName}
-&
-numOfRows=10
-&
-pageNo=1`;
+const example = `http://apis.data.go.kr/6300000/eventDataService/eventDataListJson?serviceKey=${keyName}&numOfRows=10&pageNo=1`;
 
 console.log(example)
